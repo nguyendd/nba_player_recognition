622a73c8a92908ba1ead245fc0bfd8ddb75d159c
dnguyen <dnguyen@configureone.com>
2018-04-26 22:56:07 -0500

    example of training model w/ alex abrines image search

--- train/nba_model_trainer.py
+++ train/nba_model_trainer.py
@@ -1,7 +1,3 @@
-#nba_players_file = open("NBA_Facial_Recognition/all_active_nba_players.txt", "r")
-#nbaPlayersWithLineBreak = nba_players_file.readLines()
-#nbaPlayers = [nbaPlayersWithLineBreak.rstrip('\n'\) for nbaPlayersWithLineBreak in open("NBA_Facial_Recognition/all_active_nba_players.txt")]
-#print(nbaPlayers)
 import urllib.request, json
 from clarifai.rest import ClarifaiApp
 from parse import getFormattedNbaPlayers
@@ -11,23 +7,40 @@ import os
 key = os.getenv('GOOGLE_SEARCH_KEY')
 cx = os.getenv('GOOGLE_SEARCH_CX')
 clarifai_key = os.getenv('CLARIFAI_KEY')
-numberOfResults = 5
-url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&num={numberOfResults}&searchType=image&q=lebron%20james"
-r = urllib.request.urlopen(url)
-json = json.loads(r.read())
+numberOfResults = 9
 
-print(json.get('items')[0].get('link'))
 
-formattedNbaPlayers = getFormattedNbaPlayers()
+# formattedNbaPlayers = getFormattedNbaPlayers()
 
 app = ClarifaiApp(api_key=clarifai_key)
 
 model = app.models.get('Nba Players')
-conceptIds = model.get_concept_ids()
+#conceptIds = model.get_concept_ids()
 
 
+# for conceptId in conceptIds:
+# 	formattedConceptId = conceptId.replace(" ", "%20")
+# 	print(formattedConceptId)
+	#url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&num={numberOfResults}&searchType=image&q=lebron%20james"
+	#r = urllib.request.urlopen(url)
+	#json = json.loads(r.read())
 
-for nbaPlayer in formattedNbaPlayers:
-	if not (nbaPlayer in conceptIds):
-		model.add_concepts([nbaPlayer])
-		time.sleep(1)
+#print(json.get('items')[0].get('link'))
+
+# for nbaPlayer in formattedNbaPlayers:
+# 	if not (nbaPlayer in conceptIds):
+# 		model.add_concepts([nbaPlayer])
+# 		time.sleep(1)
+
+
+url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&num={numberOfResults}&searchType=image&q=Alex%20Abrines"
+r = urllib.request.urlopen(url)
+json = json.loads(r.read())
+googleImageSearchItems = json.get('items')
+imageLinks = list(map(lambda item: item.get('link'), googleImageSearchItems))
+
+for imageLink in imageLinks:
+	app.inputs.create_image_from_url(url=imageLink, concepts=['Alex Abrines'])
+
+
+model.train()	
