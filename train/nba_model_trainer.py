9cda61d5aa7421fbe33abf8fd2236dd369edc928
dnguyen <dnguyen@configureone.com>
2018-04-26 23:10:56 -0500

    trains model by list of players

--- train/nba_model_trainer.py
+++ train/nba_model_trainer.py
@@ -7,7 +7,7 @@ import os
 key = os.getenv('GOOGLE_SEARCH_KEY')
 cx = os.getenv('GOOGLE_SEARCH_CX')
 clarifai_key = os.getenv('CLARIFAI_KEY')
-numberOfResults = 9
+numberOfResults = 10
 
 
 # formattedNbaPlayers = getFormattedNbaPlayers()
@@ -17,30 +17,19 @@ app = ClarifaiApp(api_key=clarifai_key)
 model = app.models.get('Nba Players')
 #conceptIds = model.get_concept_ids()
 
+untrainedConcepts = ['Bradley Beal', 'John Wall', 'Quincy Acy', 'Steven Adams']
 
-# for conceptId in conceptIds:
-# 	formattedConceptId = conceptId.replace(" ", "%20")
-# 	print(formattedConceptId)
-	#url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&num={numberOfResults}&searchType=image&q=lebron%20james"
-	#r = urllib.request.urlopen(url)
-	#json = json.loads(r.read())
+for conceptId in untrainedConcepts:
+	formattedConceptId = conceptId.replace(" ", "%20")
+	url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&num={numberOfResults}&searchType=image&q={formattedConceptId}"
+	r = urllib.request.urlopen(url)
+	json_data = json.loads(r.read())
+	googleImageSearchItems = json_data.get('items')
+	imageLinks = list(map(lambda item: item.get('link'), googleImageSearchItems))
+	for imageLink in imageLinks:
+		app.inputs.create_image_from_url(url=imageLink, concepts=[conceptId])
+		time.sleep(2)
 
 #print(json.get('items')[0].get('link'))
-
-# for nbaPlayer in formattedNbaPlayers:
-# 	if not (nbaPlayer in conceptIds):
-# 		model.add_concepts([nbaPlayer])
-# 		time.sleep(1)
-
-
-url = f"https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&num={numberOfResults}&searchType=image&q=Alex%20Abrines"
-r = urllib.request.urlopen(url)
-json = json.loads(r.read())
-googleImageSearchItems = json.get('items')
-imageLinks = list(map(lambda item: item.get('link'), googleImageSearchItems))
-
-for imageLink in imageLinks:
-	app.inputs.create_image_from_url(url=imageLink, concepts=['Alex Abrines'])
-
 
 model.train()	
